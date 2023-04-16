72e9f89d8659151bc9eb8e7baf7791c75a767dae
Hoja Tesema <hojats@hotmail.com>
2023-04-16 03:59:46 -0700

    1.c

---
+++ header.h
@@ -0,0 +1,14 @@
+#ifndef HEADER_H
+#define HEADER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <ctype.h>
+
+#int interactive(info_t *info);
+#int is_delim(char c, char *delim);
+#int _isalpha(int c);
+#int _atoi(char *s);
+
+#endif
